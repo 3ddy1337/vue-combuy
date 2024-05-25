a68a85c62af9585be70c5b80afd88918c39fc957
3ddy1337 <em@2mrk.de>
2024-05-25 10:00:55 +0200

    changes

--- src/views/MusthaveInquiry.vue
+++ src/views/MusthaveInquiry.vue
@@ -1,25 +1,32 @@
 <template>
-  <h2>Frage einen Musthave-Artikel an!</h2>
+  <div class="wrapper">
+    <h2>Musthave</h2>
+    <p>
+      Hier kannst du deinen Wunschartikel anlegen. Nachdem wir deine Anfrage
+      geprüft haben, werden in der Kategorie "Musthave" die Artikel angezeigt,
+      die du dir wünschst.
+    </p>
+  </div>
   <form>
-    <div><label for="name">Produktname: </label></div>
-
+    <label for="name">Produktname: </label>
     <input type="text" id="name" name="name" required />
-    <label for="email">Hersteller: </label>
-    <input type="email" id="email" name="email" required />
-    <label for="musthave">EAN: </label>
-    <input type="text" id="musthave" name="musthave" required />
+    <label for="manufacturer">Hersteller: </label>
+    <input type="text" id="manufacturer" name="manufacturer" required />
+    <label for="ean">EAN: </label>
+    <input type="text" id="ean" name="ean" />
     <label for="color">Wunschfarbe: </label>
-    <input type="text" id="color" name="color" required />
-    <label for="musthave">Hersteller-Link: </label>
-    <input type="text" id="herstellerlink" name="herstellerlink" required />
+    <input type="text" id="color" name="color" />
+    <label for="manufacturerlink">Hersteller-Link: </label>
+    <input type="text" id="manufacturerlink" name="manufacturerlink" />
     <label for="message">Nachricht:</label>
-    <textarea id="message" name="message" required></textarea>
+    <textarea id="message" name="message"></textarea>
+    <div></div>
     <button type="submit">Absenden</button>
   </form>
 </template>
 
 <style scoped>
-h2 {
+.wrapper {
   box-shadow: 0 0 5px 0 rgba(0, 0, 0, 0.3);
   border-radius: 3px;
   background: linear-gradient(
@@ -36,18 +43,45 @@ h2 {
 form {
   font-family: "Exo 2";
   display: grid;
-  grid-template-columns: 1fr 1fr;
+  grid-template-columns: auto 1fr;
   gap: 10px;
   margin: 10px;
-  text-align: left;
-  justify-content: start;
-  justify-items: start;
+  box-shadow: 0 0 5px 0 rgba(0, 0, 0, 0.3);
+  border-radius: 3px;
+  background: linear-gradient(
+    125deg,
+    rgb(209, 253, 255) 0%,
+    rgba(42, 255, 251, 0.418) 40%,
+    rgb(133, 228, 254) 100%
+  );
+  padding: 10px;
 }
 
-div {
-  text-align: left;
-  justify-self: start;
-  justify-content: start;
-  justify-items: start;
+button {
+  font-family: "Nico Moji";
+  background-color: white;
+  border: none;
+  padding: 10px;
+  border-radius: 3px;
+  box-shadow: 0 0 5px 0 rgba(0, 0, 0, 0.3);
+}
+
+p {
+  font-family: "Exo 2";
+  margin: 10px;
+}
+
+label {
+  margin-left: 10px;
+}
+
+input {
+  width: 180px;
+  height: 21px;
+}
+
+textarea {
+  width: 180px;
+  height: 42px;
 }
 </style>
